4f8eee8e0406f8dcdafb376dee12d0c6eb17789f
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-02-26 07:51:49 -0800

    postgrator config

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,12 +5,9 @@ pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: 
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.MIGRATION_DATABASE=_HOST,
+  "host": process.env.MIGRATION_DATABASE_HOST,
   "port": process.env.MIGRATION_DATABASE_PORT,
   "database": process.env.MIGRATION_DATABASE_NAME,
   "username": process.env.MIGRATION_DATABASE_USER,
   "password": process.env.MIGRATION_DATABASE_PASS,
-  "connectionString": (process.env.NODE_ENV === 'test')
-  ? process.env.TEST_DATABASE_URL
-  : process.env.DATABASE_URL,
 }
