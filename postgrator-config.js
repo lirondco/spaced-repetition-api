60965db2a126090779c3e2e1361e56041a8a287c
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-02-26 07:56:23 -0800

    ssl again

--- postgrator-config.js
+++ postgrator-config.js
@@ -10,4 +10,5 @@ module.exports = {
   "database": process.env.MIGRATION_DATABASE_NAME,
   "username": process.env.MIGRATION_DATABASE_USER,
   "password": process.env.MIGRATION_DATABASE_PASS,
+  "ssl": true
 }
