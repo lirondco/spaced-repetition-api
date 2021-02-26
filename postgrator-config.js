46d37fc7f058cd360e5a3e2be14df5d37c10cbe2
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-02-26 08:03:34 -0800

    ssl error

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,9 +5,7 @@ pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: 
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.MIGRATION_DATABASE_HOST,
-  "port": process.env.MIGRATION_DATABASE_PORT,
-  "database": process.env.MIGRATION_DATABASE_NAME,
-  "username": process.env.MIGRATION_DATABASE_USER,
-  "password": process.env.MIGRATION_DATABASE_PASS
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 }
